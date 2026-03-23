83eb9b3e40b3aaf81c8d75b84f16e7076c74778e
MovelikeLo <loicrobert640@gmail.com>
2026-03-23 11:51:58 -0400

    make function for shuffle

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const {readFile} = require('fs').promises;
 
 
 
+router.get('/',(req,res)=>{
+    res.send('Quiz List');
+});
+
+let getWords = async () => {
+    let randomPart = getRandomPart();
+
+}
+let getRandomPart = async () => {
+    let parts = ['noun','verb','adjective'];
+    let randomIndex = Math.floor(Math.random() * parts.length);
+    let randompart = parts[randomIndex];
+    return randompart;
+}
+let shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        let randomNumber = Math.floor(Math.random() * (i + 1));
+        [array[i], array[randomNumber]] = [array[randomNumber], array[i]];
+    }
+};
 
 
-
-
-
-
-module
+module.exports = router;
