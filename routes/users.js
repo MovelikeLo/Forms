f0719dad844150b0e28636082ae15c7494107c7b
MovelikeLo <loicrobert640@gmail.com>
2026-03-23 11:33:21 -0400

    created quiz router

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/',(req,res)=>{
+    res.send('User List');
+});
+router.get('/new',(req,res)=>{
+    res.send(`User New Form`);
+});
+router.route('/:id').get((req, res)=>{
+res.send(`Getting User data for id: ${req.params.id}`);
+}).delete((req, res)=>{
+res.send(`Deleting User data for id: ${req.params.id}`);
+}).put((req, res)=>{
+res.send(`Updating User data for id: ${req.params.id}`);
+});
+
+module.exports = router;
