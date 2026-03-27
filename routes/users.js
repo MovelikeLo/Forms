86cb37f061a486e8976ecfd6f0e5163f92f7be44
MovelikeLo <loicrobert640@gmail.com>
2026-03-27 12:52:38 -0400

    Added Last Name, Age and Gender

--- routes/users.js
+++ routes/users.js
@@ -7,15 +7,19 @@ router.get('/',(req,res)=>{
     res.send('User List');
 }).post('/',(req,res)=>{
    const firstName = req.body.firstName;
-   const isValid = firstName !=="";
+   const lastName = req.body.lastName;
+   const age = req.body.age;
+   const gender = req.body.gender;
+
+   const isValid = firstName !=="" && lastName !=="" && age !=="" && gender !=="";
     if(isValid){
-        console.log(`Adding user: ${firstName}`);
-        users.push({firstName});
+        console.log(`Adding user: ${firstName} ${lastName}, ${age}, ${gender}`);
+        users.push({firstName, lastName, age, gender});
         res.render('users/list',{users});
     }
     else{
-        console.log('Error: First Name is required');
-        res.render('users/new',{firstName});
+        console.log('Error');
+        res.render('users/new',{firstName, lastName, age, gender});
     }
 });
 router.get('/list',(req,res)=>{
@@ -34,7 +38,7 @@ res.send(`Deleting User data for id: ${req.params.id}`);
 res.send(`Updating User data for id: ${req.params.id}`);
 });
 
-const users =[{firstName:"George"}, {firstName:"Justyna"}];
+const users =[{firstName:"George", lastName:"Washington", age:25, gender:"Male"}, {firstName:"Justyna", lastName:"Kean", age:30, gender:"Female"}];
 
 router.param('id',(req,res,next,id)=>{
     req.user = users[id];
